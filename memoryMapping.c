cb2d6479c31dd9695d3fb4979d249d917bf8399f
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-03-25 19:01:37 -0400

    adding the final bit of code.

--- memoryMapping.c
+++ memoryMapping.c
@@ -8,7 +8,7 @@ char globalMessage[] = "Hello";
 
 void printFunc() {
     int localVarible = 20;
-    printf("local variable: %d, %p\n", localVarible, &localVarible);
+    printf("Local variable to the print function: %p\n", &localVarible);
 }
 
 int factorial(int number) {
@@ -16,7 +16,7 @@ int factorial(int number) {
         return 1;
     }
     else {
-        printf("Direction of growth stack (down): %p\n", &number);
+        printf("Direction of growth stack: %p\n", &number);
         return number * factorial(number -1);
     }
 }
@@ -27,17 +27,19 @@ int main() {
     char localChar = 's';
     float localFloat = 1.36;
     staticVariable = 10;
-    int *ptr = (int*) malloc(sizeof(int) * 10);
+    int *intPtr = (int*) malloc(sizeof(int) * 10);
     *intPtr = 50;
     char *charPtr = (char*) malloc(sizeof(char) * 10);
     *charPtr = 'w';
     float *floatPtr = (float*) malloc(sizeof(float) * 10);
     *floatPtr= 1.90;
-    printf("local variable (initialized data segment): int: %p, char: %p, float: %p\n", &staticVariable, &localChar, &localFloat);
+    printf("Shared C libraries: stdio.h: %p, sys/resource.h: %p, sys/time.h: %p, stdlib.h: %p\n", &puts, &getrusage, &gettimeofday, &qsort);
+    printf("Address of the print function (program text): %p\n", &printFunc);
+    printf("Local variable (initialized data segment): int: %p, char: %p, float: %p\n", &staticVariable, &localChar, &localFloat);
     printf("Global message (initialized data segment): %p\n", &globalMessage);
-    printf("local uninitialized variable: %p\n", &uninitializedVariable);
+    printf("Local uninitialized variable: %p\n", &uninitializedVariable);
     printf("Global variable (uninitialized data segment): %p\n", &globalVariable);
-    printf("Heap addresses: %p, %p, %p\n",(void*)intPtr, (void*)charPtr, (void*)floatPtr);
+    printf("Heap addresses (run-time data): %p, %p, %p\n",(void*)intPtr, (void*)charPtr, (void*)floatPtr);
     printFunc();
     free(intPtr);
     free(charPtr);
